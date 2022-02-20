ca170c43614ae9327b7a99f17e19aa39507aa724
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-02-20 10:53:45 -0500

    Changed default display
    Calculator screen displays '0' as default

--- script.js
+++ script.js
@@ -12,45 +12,80 @@ let nums = el(".num");
 let output = el("#output");
 let clear = el("#clear");
 let currNum = "";
-let oldNum = "";
-let resultNum = "";
+let oldNum, resultNum;
 
-//display current number to output screen
-const setNum = function () {
+function init() {
+  addNumberListeners();
+  addClearListener();
+}
+
+//Add key event listeners
+function addNumberListeners() {
+  nums.forEach((numKey) => {
+    numKey.addEventListener("click", displayInput);
+  });
+}
+
+function addClearListener() {
+  clear.addEventListener("click", clearNums);
+}
+
+function display(num) {
+  output.innerHTML = num;
+}
+
+function displayInput() {
   if (resultNum) {
-    //if result previously displayed, clear and reset result
     currNum = this.getAttribute("data-num");
     resultNum = "";
   } else if (!resultNum && currNum.length < 12) {
-    //add current number if number length <= 12
-    if (this.getAttribute("data-num") === ".") {
-      //if input is decimal, check that there are no other decimal points in the current number already
-      if (!currNum.includes(".")) {
-        currNum += this.getAttribute("data-num");
-      }
-    } else {
-      //for every other number
-      currNum += this.getAttribute("data-num");
-    }
-  } else {
-    //do nothing if number length > 12
-    null;
+    currNum += this.getAttribute("data-num");
   }
-  output.innerHTML = currNum;
-};
+  display(currNum);
+}
 
-//clear button
-const clearCalc = function () {
-  currNum = "";
+function clearNums() {
+  currNum = "0";
   oldNum = "";
   resultNum = "";
-  output.innerHTML = currNum;
-};
+  display(currNum);
+}
 
-//add click events to numbers
-nums.forEach(function (element) {
-  element.onclick = setNum;
-});
+init();
 
-//add click event for clear
-clear.onclick = clearCalc;
+// //display current number to output screen
+// const setNum = function () {
+//   if (resultNum) {
+//     //if result previously displayed, clear and reset result
+//     currNum = this.getAttribute("data-num");
+//     resultNum = "";
+//   } else if (!resultNum && currNum.length < 12) {
+//     //add current number if number length <= 12
+//     if (this.getAttribute("data-num") === ".") {
+//       //if input is decimal, check that there are no other decimal points in the current number already
+//       if (!currNum.includes(".")) {
+//         currNum += this.getAttribute("data-num");
+//       }
+//     } else {
+//       //for every other number
+//       currNum += this.getAttribute("data-num");
+//     }
+//   }
+//   output.innerHTML = currNum;
+// };
+
+// //clear button
+// const clearCalc = function () {
+//   currNum = "";
+//   oldNum = "";
+//   resultNum = "";
+//   output.innerHTML = currNum;
+// };
+
+// //add click events to numbers
+// nums.forEach(function (element) {
+//   element.onclick = setNum;
+// });
+
+// //add click event for clear
+// clear.onclick = clearCalc;
